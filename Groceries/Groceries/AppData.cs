c2a6f9dd79a6ab6bb3c16723a87e67ebd31aee4e
Soumik Patel <patel.soumik4@gmail.com>
2017-12-27 20:42:16 +1300

    lecture9

---
+++ Groceries/Groceries/AppData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Groceries
+{
+    public class AppData
+    {
+
+        public static UserClass currentUser;
+        public static List<GroceryListClass> currentLists;
+
+        private static AppData instance;
+
+
+        public static AppData GetInstance ()
+        {
+            if (instance == null )
+            {
+                instance = new AppData();
+            }
+
+
+            return instance;
+        }
+
+
+    }
+}
